06d3780c8187caa6adabdb0971c44aeb6bd157ae
FStriewski <fstriewski@outlook.com>
2018-04-23 20:05:56 +0200

    Style TopBar

--- src/components/ui/TopBar.js
+++ src/components/ui/TopBar.js
@@ -6,33 +6,96 @@ import LogOutPage from '../userHandling/LogOutPage'
 import SignUpPage from '../userHandling/SignUpPage'
 import ProductsList from '../ProductsList'
 import { Link } from 'react-router-dom'
-import { Navbar, NavItem, Nav,   } from 'react-bootstrap';
+import * as combine from "lodash/fp/compose"
 
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import IconButton from 'material-ui/IconButton';
+import Switch from 'material-ui/Switch';
+import { FormControlLabel, FormGroup } from 'material-ui/Form';
+import Menu, { MenuItem } from 'material-ui/Menu';
+import Dehaze from '@material-ui/icons/Dehaze'
 
- export default class TopBar extends PureComponent {
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
 
-   render() {
-     return (
-       <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-             <Link to={'/products'} component={ProductsList} className="products">WebShop</Link>
-            </Navbar.Brand>
-          </Navbar.Header>
-  
-         <Nav pullRight>
-            <NavItem eventKey={1}>
-             <Link to={'/login'} component={LogInPage} className="login">Log In</Link>
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-             <Link to={'/logout'} component={LogOutPage} className="logout">Log Out</Link>
-            </NavItem>
-           <NavItem eventKey={3} href="#">
-             <Link to={'/signup'} component={SignUpPage} className="signup">Sign Up</Link>
-            </NavItem>
-          </Nav>
- 
-        </Navbar>
-     )
-    }
+class TopBar extends React.Component {
+  state = {
+    auth: true,
+    anchorEl: null,
+  };
+
+  handleChange = (event, checked) => {
+    this.setState({ auth: checked });
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { auth, anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              <Link to={'/products'} component={ProductsList} className="products">WebShop 3000</Link>
+            </Typography>
+          
+              <div>  
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+                <Dehaze />
+              </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={open}
+                  onClose={this.handleClose}
+                >
+                  <MenuItem onClick={this.handleClose}><Link to={'/login'} component={LogInPage} className="login">Log In</Link></MenuItem>
+                  <MenuItem onClick={this.handleClose}><Link to={'/logout'} component={LogOutPage} className="logout">Log Out</Link></MenuItem>
+                  <MenuItem onClick={this.handleClose}><Link to={'/signup'} component={SignUpPage} className="signup">Sign Up</Link></MenuItem>
+                </Menu>
+              </div>
+          
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
   }
+}
+
+export default withStyles(styles)(TopBar);
